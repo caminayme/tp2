683865fb2385ab0b42640fcc7aa916990ac0d759
caminayme <camilanayme@gmail.com>
2022-09-26 19:57:41 -0300

    heroku2

--- app.js
+++ app.js
@@ -10,10 +10,6 @@ app.listen(process.env.PORT || 3000, function() {
     console.log('Servidor corriendo')
 });
 
-app.listen(process.env.PORT || 3000, function() {
-    console.log("Servidor corriendo");
-})
-
 
 app.get('/', (req, res) => (
     res.sendFile(path.resolve(__dirname, './views/home.html'))
